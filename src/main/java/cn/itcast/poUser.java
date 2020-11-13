6786d6bd9821a8f25a2ea3041288b176465492df
renxian <xian1995106@gmail.com>
2020-11-14 00:47:01 +0100

    init gittest project

---
+++ src/main/java/cn/itcast/poUser.java
@@ -0,0 +1,8 @@
+package cn.itcast;
+
+/**
+ * @author renxian
+ * @date 2020-11-14-00:28
+ */
+public class poUser {
+}
---
+++ src/test/java/cn/itcast/test/UserTest.java
@@ -0,0 +1,8 @@
+package cn.itcast.test;
+
+/**
+ * @author renxian
+ * @date 2020-11-14-00:29
+ */
+public class UserTest {
+}
